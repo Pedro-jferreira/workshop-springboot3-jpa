a43270679c1c0d06688ea081a0feb576a433e3bd
Pedro Ferreira <pedrojferreiradev@gmail.com>
2023-07-18 10:49:56 -0300

    Service Layer, component registration

---
+++ src/main/java/com/pedrojferreiradev/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pedrojferreiradev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrojferreiradev.course.Repositories.UserRepository;
+import com.pedrojferreiradev.course.entities.User;
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}

93eed0f572f01723ab8ac6ed49810187a1d3cc43
Pedro Ferreira <pedrojferreiradev@gmail.com>
2023-07-30 16:47:55 -0300

    Exeption handling - update

--- src/main/java/com/pedrojferreiradev/course/services/UserService.java
+++ src/main/java/com/pedrojferreiradev/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.pedrojferreiradev.course.entities.User;
 import com.pedrojferreiradev.course.services.exceptions.DatabaseException;
 import com.pedrojferreiradev.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -36,17 +38,20 @@ public class UserService {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-			e.printStackTrace();
-			
+			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 	
-	public User update(Long id, User objt) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity , objt);
-		return repository.save(entity);
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 	
 	private void updateData(User entity, User objt) {

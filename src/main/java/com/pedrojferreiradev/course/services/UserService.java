cbd9608db1cdd62c254fd20f82b1c4fa1e90fb4e
Pedro Ferreira <pedrojferreiradev@gmail.com>
2023-07-30 15:58:50 -0300

    Exception hadling - findbyid

--- src/main/java/com/pedrojferreiradev/course/services/UserService.java
+++ src/main/java/com/pedrojferreiradev/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.pedrojferreiradev.course.Repositories.UserRepository;
 import com.pedrojferreiradev.course.entities.User;
+import com.pedrojferreiradev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 
 	}
 
